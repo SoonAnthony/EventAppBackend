f9feaabc9bc3ddce596398da7ca4186148be5329
SoonAnthony <anthonymedia062@gmail.com>
2026-09-11 21:54:44 +0300

    Fix: Service layer now uses a custom exception

--- app/user/services.py
+++ app/user/services.py
@@ -2,14 +2,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import EmailStr
 from app.user.models import User
 from sqlalchemy import select
-from fastapi import HTTPException
-
-
+from app.user.exceptions import UserNotFoundError
 
 async def get_user_by_email(session:AsyncSession, user_email:EmailStr)-> User:
     result = await session.execute(select(User).where(User.email == user_email))
     user = result.scalars().one_or_none()
-    if not user:
-        raise HTTPException(status_code=404, detail="No user found!")
-    else:
-        return user
+    if user is None:
+        raise UserNotFoundError(user_email)
+    return user
