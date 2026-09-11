3fb192acc25c5826db492e8af6ebd39e2f30caff
SoonAnthony <anthonymedia062@gmail.com>
2026-09-11 22:17:22 +0300

    Add: UserNotFoundByIdError

--- app/user/exceptions.py
+++ app/user/exceptions.py
@@ -1,6 +1,11 @@
-
+from uuid import UUID
 
 class UserNotFoundError(Exception):
     def __init__(self, email:str):
         self.email = email
-        super().__init__("No user found with {email}")
+        super().__init__(f"No user found with email: {email}")
+
+class UserNotFoundByIdError(Exception):
+    def __init__(self, id:UUID):
+        self.id = id
+        super().__init__(f"No user found with id: {id}")
